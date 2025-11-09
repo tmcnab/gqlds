03127bd37f4da338d99a95c4dd5bfab4a38b3b23
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2025-11-09 02:43:09 -0500

    Enhance createQueryType by adding argument support for order and direction in GraphQL queries; update field naming to maintain consistency.

--- src/createQueryType.ts
+++ src/createQueryType.ts
@@ -1,5 +1,5 @@
 import Database from 'better-sqlite3';
-import { GraphQLFieldConfig, GraphQLList, GraphQLObjectType } from 'graphql'
+import { GraphQLFieldConfig, GraphQLFieldConfigArgumentMap, GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql'
 import { TableInfo } from "./types/TableInfo"
 import { createTypes } from './createTypes'
 
@@ -7,13 +7,19 @@ export const createQueryType = (items: TableInfo[]): GraphQLObjectType => {
     const types = createTypes(items)
     const queryType: GraphQLObjectType = new GraphQLObjectType({
         fields: items.reduce((value, table) => {
+            const args: GraphQLFieldConfigArgumentMap = {
+                order: { type: GraphQLString },
+                direction: { type: GraphQLString },
+            }
+
             const config: GraphQLFieldConfig<any, any, any> = {
+                args,
                 resolve: (source, args, context, info) => {
                     return new Database('Chinook.sqlite').prepare(`SELECT * FROM ${table.name}`).all()
                 },
-                type: new GraphQLList(types.find(t => t.name == table.name) as GraphQLObjectType) // TODO
+                type: new GraphQLList(types.find(t => t.name == table.name) as GraphQLObjectType),
             }
-            value[table.name.toLocaleLowerCase()] = config
+            value[table.name] = config
             return value
         }, {}),
         name: 'Query',
