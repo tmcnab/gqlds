4f41ccf3c54be4f200684a76371ffd6ccff13010
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2025-11-11 00:44:55 -0500

    add skip take args to query type

--- src/functions/createQueryType.ts
+++ src/functions/createQueryType.ts
@@ -1,32 +1,42 @@
 import { createTypes } from './createTypes'
-import { GraphQLFieldConfig, GraphQLFieldConfigArgumentMap, GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql'
+import { GraphQLFieldConfig, GraphQLFieldConfigArgumentMap, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql'
 import { TableInfo } from "../types/TableInfo"
 import Database from 'better-sqlite3';
 
 // https://hasura.io/docs/2.0/api-reference/graphql-api/query/
-// https://hasura.io/docs/2.0/api-reference/graphql-api/mutation/
 
 export const createQueryType = (items: TableInfo[]): GraphQLObjectType => {
-    const types = createTypes(items)
-    const queryType: GraphQLObjectType = new GraphQLObjectType({
-        fields: items.reduce((value, table) => {
-            const args: GraphQLFieldConfigArgumentMap = {
-                order: { type: GraphQLString },
-                direction: { type: GraphQLString },
-            }
+	const types = createTypes(items)
+	const queryType: GraphQLObjectType = new GraphQLObjectType({
+		fields: items.reduce((value, table) => {
+			const args: GraphQLFieldConfigArgumentMap = {
+				take: { type: GraphQLInt },
+				skip: { type: GraphQLInt },
+			}
 
-            const config: GraphQLFieldConfig<any, any, any> = {
-                args,
-                resolve: (source, args, context, info) => {
-                    return new Database('Chinook.sqlite').prepare(`SELECT * FROM ${table.name}`).all()
-                },
-                type: new GraphQLList(types.find(t => t.name == table.name) as GraphQLObjectType),
-            }
-            value[table.name] = config
-            return value
-        }, {}),
-        name: 'Query',
-    })
+			const config: GraphQLFieldConfig<any, any, any> = {
+				args,
+				resolve: (source, args, context, info) => {
+					console.log(args)
+					let sql = `SELECT * FROM ${table.name}`
+					if (args['take']) {
+						sql = `${sql} LIMIT ${args['take']}`
+					}
+					if (args['skip']) {
+						sql = `${sql} OFFSET ${args['skip']}`
+					}
 
-    return queryType
+					console.log(sql)
+
+					return new Database('Chinook.sqlite').prepare(sql).all()
+				},
+				type: new GraphQLList(types.find(t => t.name == table.name) as GraphQLObjectType),
+			}
+			value[table.name] = config
+			return value
+		}, {}),
+		name: 'Query',
+	})
+
+	return queryType
 }
