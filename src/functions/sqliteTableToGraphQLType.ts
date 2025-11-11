ee8e81a4f0902d4159bb8a653270fd269cdc1504
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2025-11-11 03:28:03 -0500

    Enhance sqliteTableToGQLType function to dynamically generate GraphQL fields from SQLite table columns using mapSqliteTypeToGQLType.

--- src/functions/sqliteTableToGraphQLType.ts
+++ src/functions/sqliteTableToGraphQLType.ts
@@ -1,9 +1,15 @@
 import { GraphQLObjectType } from 'graphql'
+import { mapSqliteTypeToGQLType } from './mapSqliteTypeToGQLType'
 import { TableInfo } from '../types/TableInfo'
 
 export const sqliteTableToGQLType = (tableInfo: TableInfo) => {
     return new GraphQLObjectType({
         name: tableInfo.name,
-        fields: {},
+        fields: tableInfo.columns.reduce((fields, column) => {
+            fields[column.name] = {
+                type: mapSqliteTypeToGQLType(column.type)
+            }
+            return fields
+        }, {} as Record<string, any>),
     })
 }
