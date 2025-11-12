81cac9e0f4b4f3654e5a889610e900ac08e781b5
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2025-11-12 00:31:30 -0500

    Remove commented-out mutation import from createSchema and update import statement for TableInfo in introspectDatabase for consistency.

--- src/functions/createSchema.ts
+++ src/functions/createSchema.ts
@@ -1,4 +1,3 @@
-// import { createMutation } from './createMutationType'
 import { createQueryType } from './createQueryType'
 import { GraphQLSchema } from 'graphql'
 import { SortDirection } from '../types/Sort'
@@ -7,7 +6,6 @@ import { TableInfo } from '../types/TableInfo'
 export const createSchema = (items: TableInfo[]) => {
     return new GraphQLSchema({
         query: createQueryType(items),
-        // mutation: createMutation(items),
         types: [
             SortDirection,
         ]
