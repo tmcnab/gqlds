909147f31ce56caa366e9cc2fa3f07a09b23d6fd
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2025-11-13 22:55:39 -0500

    Refactor sqlTableToGqlType to use ThunkObjMap for fields and improve foreign key handling

--- src/functions/sqlTableToGqlType.ts
+++ src/functions/sqlTableToGqlType.ts
@@ -1,15 +1,26 @@
-import { GraphQLObjectType } from 'graphql'
-import { sqlTypeToGqlType } from './sqlTypeToGQLType'
+import {
+	GraphQLFieldConfig,
+	GraphQLObjectType,
+	ThunkObjMap,
+} from 'graphql'
 import { SqliteTable } from '../types/SqliteTable'
+import { sqlTypeToGqlType } from './sqlTypeToGQLType'
 
 export const sqlTableToGqlType = (table: SqliteTable) => {
-    return new GraphQLObjectType({
-        name: table.name,
-        fields: table.columns.reduce((fields, column) => {
-            fields[column.name] = {
-                type: sqlTypeToGqlType(column.type)
-            }
-            return fields
-        }, {} as Record<string, any>),
-    })
+	const fields: ThunkObjMap<GraphQLFieldConfig<any, any, any>> = Object.create(null)
+
+	table.columns.forEach(column => {
+		fields[column.name] = {
+			type: sqlTypeToGqlType(column.type)
+		}
+	})
+
+	table.foreignKeys.forEach(fk => {
+		// TODO
+	})
+
+	return new GraphQLObjectType({
+		fields,
+		name: table.name,
+	})
 }
