543e8ac7ba89a33b8f0a241a160a14e9684f9150
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2025-12-02 00:21:00 -0500

    Update VSCode settings and format TableInfo interface for consistency

--- src/functions/tableInfo.ts
+++ src/functions/tableInfo.ts
@@ -1,18 +1,18 @@
 import { Database } from 'better-sqlite3'
 
 export interface TableInfo {
-    columns: {
-        name: string
-        nullable: boolean
-        primaryKey: boolean
-        type: string
-    }[]
-    foreignKeys: {
-        domesticColumn: string
-        foreignColumn: string
-        foreignTable: string
-    }[]
-    name: string
+	columns: {
+		name: string
+		nullable: boolean
+		primaryKey: boolean
+		type: string
+	}[]
+	foreignKeys: {
+		domesticColumn: string
+		foreignColumn: string
+		foreignTable: string
+	}[]
+	name: string
 }
 
 export const tableInfo = (db: Database, name: string): TableInfo => {
