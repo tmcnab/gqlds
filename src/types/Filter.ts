e34921d7e6d43e4315442fe4e5478f06d4aa5866
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2025-12-01 22:00:27 -0500

    update FilterCriteria to accept multiple value types

--- src/types/Filter.ts
+++ src/types/Filter.ts
@@ -1,40 +1,65 @@
-import { GraphQLBoolean, GraphQLFloat, GraphQLInt, GraphQLUnionType } from 'graphql'
+import { GraphQLBoolean, GraphQLFloat, GraphQLInt, GraphQLUnionType, GraphQLScalarType, Kind } from 'graphql'
 import {
-	GraphQLEnumType,
-	GraphQLInputObjectType,
-	GraphQLList,
-	GraphQLNonNull,
-	GraphQLString,
+    GraphQLEnumType,
+    GraphQLInputObjectType,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLString,
 } from 'graphql'
 
 export const Comparator = new GraphQLEnumType({
-	name: 'Comparator',
-	values: {
-		EQ: { value: '=' },
-		GT: { value: '>' },
-		GTE: { value: '>=' },
-		// ISN: { value: 'isnull' },
-		LT: { value: '<' },
-		LTE: { value: '<=' },
-		NEQ: { value: '<>' },
-	}
+    name: 'Comparator',
+    values: {
+        EQ: { value: '=' },
+        GT: { value: '>' },
+        GTE: { value: '>=' },
+        LT: { value: '<' },
+        LTE: { value: '<=' },
+        NEQ: { value: '<>' },
+    }
+})
+
+export const FilterValue = new GraphQLScalarType({
+    name: 'FilterValue',
+    description: 'A scalar type that can represent String, Int, Float, Boolean or null values',
+    serialize(value) {
+        return value
+    },
+    parseValue(value) {
+        return value
+    },
+    parseLiteral(ast) {
+        switch (ast.kind) {
+            case Kind.STRING:
+                return ast.value
+            case Kind.INT:
+                return parseInt(ast.value, 10)
+            case Kind.FLOAT:
+                return parseFloat(ast.value)
+            case Kind.BOOLEAN:
+                return ast.value
+            case Kind.NULL:
+                return null
+            default:
+                throw new Error(`Unsupported kind: ${ast.kind}`)
+        }
+    }
 })
 
 
 export const FilterCriteria = new GraphQLInputObjectType({
-	fields: {
-		field: {
-			type: new GraphQLNonNull(GraphQLString),
-		},
-		operator: {
-			type: new GraphQLNonNull(Comparator),
-		},
-		value: {
-			// TODO: make a union type
-			type: new GraphQLNonNull(GraphQLString),
-		}
-	},
-	name: 'FilterCriteria',
+    fields: {
+        field: {
+            type: new GraphQLNonNull(GraphQLString),
+        },
+        operator: {
+            type: new GraphQLNonNull(Comparator),
+        },
+        value: {
+            type: FilterValue,
+        }
+    },
+    name: 'FilterCriteria',
 })
 
 export const FilterCriteriaList = new GraphQLList(FilterCriteria)
